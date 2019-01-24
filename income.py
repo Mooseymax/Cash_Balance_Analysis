d2edaf09c948ad93a365183610fced640b49f644
Mooseymax <mooseandham@gmail.com>
2019-01-24 01:59:26 +0000

    Almost complete

--- income.py
+++ income.py
@@ -3,11 +3,11 @@ import os.path
 from datetime import datetime
 from openpyxl import Workbook
 from openpyxl import load_workbook
-from module.client import Client, Account
+from module.client import Client, Account, Fund, Transaction
 
 '''
 TRANSACTION COLUMNS
-Adviser Name, Client Reference, Client First Names, Client Surname, Product,
+Advised Ref, Adviser Name, Client Reference, Client First Names, Client Surname, Product,
 Transaction Datetime, Effective Date, Settlement Date, Transaction Description, Asset Name,
 Transaction Source, Transaction Amount, Quantity, Dealing Charge, Stamp Duty,
 Reference, Venue, Currency Amount, Currency, Book Cost,
@@ -41,7 +41,7 @@ print('Clients imported.')
 transactions = import_csv('test-t.csv')
 print('Transactions imported.')
 
-for v in valuation:
+for v in valuation[1:]:
     existing_client = False
     
     # Loop through all existing clients to check for match
@@ -58,11 +58,40 @@ for v in valuation:
     if(existing_client == False):
         # Create new client
         client_list.append(Client(v[0], v[1], v[2], v[3], v[4]))
-        #print('New client found: ' + v[3] + ' ' + v[4])
+        print('New client found: ' + v[3] + ' ' + v[4])
 
-print(len(client_list))
-print(client_list[1].fn)
-print(len(client_list[1].accounts))
+print('Total clients found: ' + str(len(client_list)))
+print('')
+
+client_list[0].details()
+
+for t in transactions[1:]:
+    existing_client = False
     
-for fund in client_list[1].accounts[0].f:
-    print(fund.n)
+    transaction = Transaction(t[5], t[6], t[9], t[10], t[11], t[12], t[13], t[21], t[22])
+    
+    for c in client_list:
+        if(t[2] == c.cr):
+            existing_client = True
+            c.add_transaction(transaction)
+            break
+        else:
+            pass
+            
+    if(existing_client == False):
+        print('Transaction not found - error')
+        input('')
+
+print('')
+print('Added transactions')
+print('')
+
+
+for c in client_list:
+    for a in c.accounts:
+        a.t.sort(key=lambda x: x.d, reverse=True)
+        newest = a.t[0]
+        # loop through transaction
+        # find first income payment
+        # divide cash account by income
+        # return how many
--- module/client.py
+++ module/client.py
@@ -18,6 +18,12 @@ class Client:
         # Additional details
         self.accounts = []
     
+    def details(self):
+        print('Client: ' + self.fn + ' ' + self.sn)
+        print('Accounts: ' + str(len(self.accounts)))
+        for i, acc in enumerate(self.accounts):
+            print('  ' + str(i) + '. ' + acc.p)
+    
     def add_fund(self, valuation):
         existing_account = False
         
@@ -27,30 +33,55 @@ class Client:
                 existing_account = True
                 a.f.append(Fund(valuation[7], valuation[8], valuation[9], valuation[10],
                             valuation[11], valuation[12], valuation[13], valuation[14]))
-                print('Account Found: ' + valuation[5])
                 break
             else:
                 # Account not found, pass for now
                 pass
+            a.update_value()
             
         if(existing_account == False):
             # Account not found - adding an account
             self.accounts.append(Account(valuation[14], valuation[5], valuation[2],
                                 valuation))
-            print('New Account Added: ' + valuation[5])
-                
-                
-                
-    
+            print(self.fn + ' ' + self.sn + ' - New Account Added: ' + valuation[5]) 
+               
     def add_transaction(self, transaction):
+        existing_account = False
+        
         for a in self.accounts:
-            if(transaction[5] == a.p):
+            if(transaction.p == a.p):
                 # Account found
                 a.t.append(transaction)
+
+                existing_account = True
+                #print('Transaction added')
+                break
             else:
                 # Account not found
                 pass
+        
+        if(existing_account == False):
+            print('Account not found - error')
+            input('')
 
+class Transaction:
+    def __init__(self, product, date, desc, name, source, amount, quantity, isin, sedol):
+        self.p = product
+        try:
+            self.d = datetime.strptime(date, '%d/%m/%Y')
+        except:
+            self.d = date
+        self.des = desc
+        self.n = name
+        self.s = source
+        self.a = float(amount)
+        if(quantity == ''):
+            self.q = 0
+        else:
+            self.q = float(quantity)
+        self.i = isin
+        self.sc = sedol
+            
 class Fund:
     def __init__(self, name, sedol, isin, cost, price, units, value, date):
         self.n = name
@@ -58,8 +89,8 @@ class Fund:
         self.i = isin
         self.c = cost
         self.p = price
-        self.u = units
-        self.v = value
+        self.u = float(units)
+        self.v = float(value)
         self.d = date
                 
 class Account:
@@ -70,15 +101,23 @@ class Account:
         
         self.f = []
         self.t = []
-        self.new_fund(transaction)
-        
+        self.i = transaction
         self.v = 0  # value
         self.c = 0  # cash
         
+        self.new_fund(Transaction(transaction[5], datetime.today(), 'Initial', 
+                        transaction[7], transaction[6], transaction[13],
+                        transaction[12], transaction[9], transaction[8]))
+        
     def update_value(self):
+        CASH = 'Investcentre SIPP Cash Account'
+        CASH_B = 'Cash GBP'
+    
         self.v = 0
         for i in self.f:
             self.v += i.v
+            if(i.n.strip() == CASH or i.n.strip() == CASH_B):
+                self.c += i.v
     
     def new_fund(self, valuation):
         GIA = 'Investcentre Dealing Account'
@@ -86,9 +125,16 @@ class Account:
         SIPP = 'Investcentre SIPP'
         LISA = 'Investcentre Lifetime ISA'
         
+        CASH = 'Investcentre SIPP Cash Account'
+        CASH_B = 'Cash GBP'
+        
+        if(valuation.n == CASH or valuation.n == CASH_B):
+            self.c += valuation.a
+        self.v += valuation.a
+        
         self.t.append(valuation)
-        self.f.append(Fund(valuation[7], valuation[8], valuation[9], valuation[10],
-                            valuation[11], valuation[12], valuation[13], valuation[14]))
+        self.f.append(Fund(self.i[7], self.i[8], self.i[9], self.i[10],
+                            self.i[11], self.i[12], self.i[13], self.i[14]))
         
         
         
